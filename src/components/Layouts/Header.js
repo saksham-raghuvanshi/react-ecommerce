b28ef25b438eb77be43be40bd8da0231e64e6fd4
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-26 16:21:45 +0530

    Product Fix

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -47,7 +47,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      <Search show={show} />
+      {show && <Search setshow={setshow} />}
     </>
   );
 };
--- src/pages/Products/ProductsList.js
+++ src/pages/Products/ProductsList.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
 import ProductCard from "../../components/Elements/ProductCard";
 import FilterBar from "./components/FilterBar";
+import { useLocation } from "react-router-dom";
 
 const ProductsList = () => {
   const [show, setShow] = useState(false);
   const [products, setProducts] = useState([]);
 
+  const search = useLocation().search;
+  const searchTerm = new URLSearchParams(search).get("q");
+  // console.log(searchTerm);
+
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch("http://localhost:8000/products");
+      const response = await fetch(
+        `http://localhost:8000/products?name_like=${
+          searchTerm ? searchTerm : ""
+        }`
+      );
       const data = await response.json();
 
       setProducts(data);
     }
     fetchProducts();
-  }, []);
+  }, [searchTerm]);
   return (
     <main className="productslist">
       <section className="my-5">
